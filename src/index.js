524be97963072a208b221d51b33c6a5afb65dd6a
Lucas Scommegna <scommegnal@hotmail.com>
2023-04-21 14:55:10 -0300

    tipos de parametros

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+// Middleware para receber um body em JSON
+app.use(express.json());
+
 /*
   GET - Buscar informação dentro do servidor
   POST - Inserir informação no servidor
@@ -10,16 +13,33 @@ const app = express();
   DELETE - Deletar informação no servidor
 */
 
+/*
+  Tipos de parâmetros:
+
+  Route Params => Identificar um recurso para o buscar/atualizar/deletar
+  Query Params => Paginação/Filtro
+  Body Params => Os objetos para inserção/alteração (JSON) deve ser informado o tipo para o express
+ */
+
 // Gets JSON data from route
 app.get("/courses", (request, response) => {
+  const query = request.query;
+
+  console.log(query);
   return response.json(["curso 1", "curso 2", "curso 3"]);
 });
 
 app.post("/courses", (request, response) => {
+  const body = request.body;
+
+  console.log(body);
   return response.json(["curso 1", "curso 2", "curso 3", "curso 4"]);
 });
 
 app.put("/courses/:id", (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
   return response.json(["curso 6", "curso 2", "curso 3", "curso 4"]);
 });
 
